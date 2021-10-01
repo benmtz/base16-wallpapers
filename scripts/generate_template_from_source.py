09350b629220b2ffed6460713bf4750d0536e431
Ben <ben.mertz.dev@gmail.com>
2021-10-01 13:08:07 +0200

    refactor: extensions & outputs

--- scripts/generate_template_from_source.py
+++ scripts/generate_template_from_source.py
@@ -2,22 +2,22 @@ from unittest import TestCase
 from os import path
 
 color_dict = {
-    "#1d1f21": "base00-hex",
-    "#282a2e": "base01-hex",
-    "#373b41": "base02-hex",
-    "#969896": "base03-hex",
-    "#b4b7b4": "base04-hex",
-    "#c5c8cf": "base05-hex",
-    "#e0e0e0": "base06-hex",
-    "#ffffff": "base07-hex",
-    "#cc6666": "base08-hex",
-    "#de935f": "base09-hex",
-    "#f0c674": "base0A-hex",
-    "#b5bd68": "base0B-hex",
-    "#8abeb7": "base0C-hex",
-    "#81a2be": "base0D-hex",
-    "#b294bb": "base0E-hex",
-    "#a3685a": "base0F-hex",
+    "1d1f21": "base00-hex",
+    "282a2e": "base01-hex",
+    "373b41": "base02-hex",
+    "969896": "base03-hex",
+    "b4b7b4": "base04-hex",
+    "c5c8cf": "base05-hex",
+    "e0e0e0": "base06-hex",
+    "ffffff": "base07-hex",
+    "cc6666": "base08-hex",
+    "de935f": "base09-hex",
+    "f0c674": "base0A-hex",
+    "b5bd68": "base0B-hex",
+    "8abeb7": "base0C-hex",
+    "81a2be": "base0D-hex",
+    "b294bb": "base0E-hex",
+    "a3685a": "base0F-hex",
 }
 
 
