14b8cfe9b924d1f25944efb98377c086cb470eda
Ben <ben.mertz.dev@gmail.com>
2021-10-01 12:44:30 +0200

    fixed templates

--- scripts/render_templates.py
+++ scripts/render_templates.py
@@ -1,6 +1,8 @@
 from os import makedirs, walk, scandir
 from os.path import dirname, join
 
+from yaml import dump
+
 from generate_template_from_source import generate_template_from_source
 
 print("Rendering mustache wallpapers")
@@ -10,7 +12,14 @@ target_dir = join(dirname(__file__), "..", "templates")
 
 makedirs(target_dir, exist_ok=True)
 
+
 with scandir(source_dir) as sources:
+
+    print(
+        dump({s.name.replace(".svg", ""): {"extension": ".svg", "output": "wallpapers"} for s in sources}),
+        file=open(join(target_dir, 'config.yaml'), 'w')
+    )
+
     for source_svg in sources:
         with open(join(source_dir, source_svg), 'r') as t:
             generated = generate_template_from_source(t.read())
