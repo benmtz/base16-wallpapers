941cb00cc23ebc192d76df51afbd3f96764ee536
Ben <ben.mertz.dev@gmail.com>
2021-10-01 13:55:55 +0200

    fix: template badly names

--- scripts/render_templates.py
+++ scripts/render_templates.py
@@ -1,3 +1,4 @@
+from itertools import tee
 from os import makedirs, walk, scandir
 from os.path import dirname, join
 
@@ -6,26 +7,26 @@ from yaml import dump
 from generate_template_from_source import generate_template_from_source
 
 print("Rendering mustache wallpapers")
-
 source_dir = join(dirname(__file__), "..", "sources")
 target_dir = join(dirname(__file__), "..", "templates")
+
+print("from {} to {}".format(source_dir, target_dir))
 
 makedirs(target_dir, exist_ok=True)
 
 
+def source_basename(s):
+    return s.name.replace(".svg", "")
+
+
 with scandir(source_dir) as sources:
+    with open(join(target_dir, 'config.yaml'), 'w') as f:
+        f.write(dump({source_basename(s): {"extension": ".svg", "output": source_basename(s)} for s in sources}))
 
-    def source_basename(s):
-        return s.name.replace(".svg", "")
-
-    print(
-        dump({source_basename(s): {"extension": ".svg", "output": source_basename(s)} for s in sources}),
-        file=open(join(target_dir, 'config.yaml'), 'w')
-    )
-
+with scandir(source_dir) as sources:
     for source_svg in sources:
+        print("Rendering {}".format(source_basename(source_svg)))
         with open(join(source_dir, source_svg), 'r') as t:
             generated = generate_template_from_source(t.read())
-            with open(join(target_dir, source_basename(s) + ".mustache"), 'w') as g:
+            with open(join(target_dir, source_basename(source_svg) + ".mustache"), 'w') as g:
                 g.write(generated)
-
