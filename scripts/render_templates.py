84382c9cd344d9807bf82aabb210edcae1eb3333
Ben <ben.mertz.dev@gmail.com>
2021-10-01 12:53:41 +0200

    refactor: extensions & outputs

--- scripts/render_templates.py
+++ scripts/render_templates.py
@@ -15,16 +15,17 @@ makedirs(target_dir, exist_ok=True)
 
 with scandir(source_dir) as sources:
 
-    name = sources.name.replace(".svg", "")
+    def source_basename(s):
+        return s.name.replace(".svg", "")
 
     print(
-        dump({name: {"extension": ".svg", "output": name} for s in sources}),
+        dump({source_basename(s): {"extension": ".svg", "output": source_basename(s)} for s in sources}),
         file=open(join(target_dir, 'config.yaml'), 'w')
     )
 
     for source_svg in sources:
         with open(join(source_dir, source_svg), 'r') as t:
             generated = generate_template_from_source(t.read())
-            with open(join(target_dir, name + ".mustache"), 'w') as g:
+            with open(join(target_dir, source_basename(s) + ".mustache"), 'w') as g:
                 g.write(generated)
 
